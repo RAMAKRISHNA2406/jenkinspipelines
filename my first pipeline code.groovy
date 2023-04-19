1247784e027e282cfed0b5574a270e149be334c3
RAMAKRISHNA2406 <veedhiramakrishna97@gmail.com>
2023-04-19 13:32:59 +0530

    github to jenkins pipeline

---
+++ my first pipeline code.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any 
+       stages {
+          stage('build') {
+             steps {
+                 sh : sleep 60 
+             }
+          }
+          stage('test') {
+             steps {
+                 echo "testing application "
+             }
+          }
+          stage('deploy') {
+            steps {
+                echo  "deploying application"
+            }
+          }
+             
+          
+       }
+    
+}
